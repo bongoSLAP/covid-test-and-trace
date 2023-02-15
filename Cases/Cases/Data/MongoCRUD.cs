2c47240946905eb51036a6086b396c2b9e588589
bongoSLAP <54848093+bongoSLAP@users.noreply.github.com>
2023-02-15 15:03:27 +0000

    setup MongoDB connection

---
+++ Cases/Cases/Data/MongoCRUD.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+
+namespace Cases.Data
+{
+    public class MongoCRUD
+    {
+        IMongoDatabase _db;
+
+        public MongoCRUD()
+        {
+            string databaseName = "cases-data";
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+               .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+               .AddJsonFile("appsettings.json")
+               .Build();
+
+            var settings = MongoClientSettings.FromConnectionString(configuration.GetConnectionString("DefaultConnection"));
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+            var client = new MongoClient(settings);
+            _db = client.GetDatabase(databaseName);
+        }
+    }
+}
