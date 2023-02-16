6a26eecd80eb59f01eb5edb2f014bdb0083221e2
bongoSLAP <54848093+bongoSLAP@users.noreply.github.com>
2023-02-16 10:57:15 +0000

    create entity models

---
+++ Cases/Cases/Models/Entities/Postcode.cs
@@ -0,0 +1,26 @@
+﻿namespace Cases.Models.Entities
+{
+    public class Postcode
+    {
+        public string Id { get; set; }
+        public string Area { get; set; }
+        public int InfectionCount { get; set; }
+        public int Population { get; set; }
+        public List<User> UsersInPostcode { get; set; }
+
+        public Postcode(
+            string id, 
+            string area, 
+            int infectionCount, 
+            int population, 
+            List<User> usersInPostcode
+        )
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+            Area = area;
+            InfectionCount = infectionCount;
+            Population = population;
+            UsersInPostcode = usersInPostcode;
+        }
+    }
+}
---
+++ Cases/Cases/Models/Entities/Venue.cs
@@ -0,0 +1,23 @@
+﻿namespace Cases.Models.Entities
+{
+    public class Venue
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public DateTime ClosingTime { get; set; }
+
+        public Venue(
+            string id, 
+            string name, 
+            string address, 
+            DateTime closingTime
+        )
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+            Name = name;
+            Address = address;
+            ClosingTime = closingTime;
+        }
+    }
+}
