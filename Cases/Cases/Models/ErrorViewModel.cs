0813c63c88666e47a70e79c0ebf747fd5b8118ef
bongoSLAP <54848093+bongoSLAP@users.noreply.github.com>
2023-02-16 10:52:39 +0000

    remove ErrorViewModel

--- Cases/Cases/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Cases.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
