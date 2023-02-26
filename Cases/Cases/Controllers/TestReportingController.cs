4f75f5e1f2ddc1563d6dfed5bdb3ebdfde370f50
jbr1gh7 <jabright377@gmail.com>
2023-02-26 15:19:01 +0000

    create test reporting controller

---
+++ Cases/Cases/Controllers/TestReportingController.cs
@@ -0,0 +1,41 @@
+﻿using Cases.Interfaces;
+using Cases.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Cases.Controllers
+{
+    public class TestReportingController : Controller
+    {
+        ITestReportingService _testReportingService;
+        public TestReportingController(ITestReportingService testReportingService)
+        {
+            _testReportingService = testReportingService;
+        }
+
+        [Authorize]
+        [HttpPost("Report")]
+        public IActionResult Report([FromBody] TestReport report)
+        {
+            if (report == null)
+                return BadRequest("Report is null");
+
+            var currentUser = HttpContext.User;
+            var usernameClaim = currentUser.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (usernameClaim == null)
+                return BadRequest("Cannot identify requester");
+
+            try
+            {
+                _testReportingService.ReportResult(report, usernameClaim.Value);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
