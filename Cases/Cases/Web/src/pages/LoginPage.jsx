f0237f49a36334e9705f18549984c42e64243291
lukejs01 <luke.jones@ada.ac.uk>
2023-02-27 16:17:53 +0000

    UI-setup login UI implemented

--- Cases/Cases/Web/src/pages/LoginPage.jsx
+++ Cases/Cases/Web/src/pages/LoginPage.jsx
@@ -1,10 +1,42 @@
-import NavBar from "../scenes/NavBar";
+import { useForm } from 'react-hook-form'
+import logo from '../assets/nhs-logo.jpeg'
+import '../styling/Login.css'
 
 const LoginPage = () => {
+
+    function LoginForm(){
+        const { register, handleSubmit, formState: {errors}} = useForm()
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <label>Enter username</label>
+                <br />
+                <input className='inputText' id='Username' type='text' placeholder='Please enter username' {...register('Username', {required: true})} />
+                {errors.Username && <p>Username is incorrect</p>}
+                <br />
+                <br />
+
+                <label>Enter password</label>
+                <br />
+                <input className='inputText' id='Password' type='password' placeholder='Pleaser enter password' {...register('Password', {required: true})} />
+                {errors.Password && <p>Password is incorrect</p>}
+                <br />
+                <br />
+                <div id='onSuccess'></div>
+
+                <input className='inputLogin' type='submit' value={"Login"} onClick={() => { document.getElementById("onSuccess").innerHTML = "Loading..." }} />
+            </form>
+        )
+    }
     return(
         <div>
-            <NavBar />
-            
+            <header>
+                <img src={logo} alt='logo' />
+            </header>
+            <h2>Welcome to Cases - NHS Track & Trace</h2>
+            <h3>Please Login to continue</h3>
+            <LoginForm />
+           
         </div>
     )
 }
