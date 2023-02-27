92cf04fe4185699596b2845182ff0d271e7031d5
luke.jones <luke.jones@callsign.com>
2023-02-27 09:42:59 +0000

    UI-setup boiler plate code and routes

---
+++ Cases/Cases/Web/src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+
+
+const LoginPage = () => {
+    return(
+        <div>
+            <div>s</div>
+            
+        </div>
+    )
+}
+
+export default LoginPage;
---
+++ Cases/Cases/Web/src/scenes/NavBar.jsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <div>
+                <h4>NHS</h4>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar;
