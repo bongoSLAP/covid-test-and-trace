ff8450a304419de8fb5bc12146305fc5fbf7daad
lukejs01 <luke.jones@ada.ac.uk>
2023-03-08 17:33:17 +0000

    UI-SymptomSurvey

--- Cases/Cases/Web/src/App.js
+++ Cases/Cases/Web/src/App.js
@@ -7,6 +7,7 @@ import VenueCheckIn from './pages/VenueCheckInPage';
 import IsolationCountdown from './pages/IsolationCountdownPage';
 import TestBooking from './pages/TestBookingPage';
 import './styling/App.css';
+import SymptomSurvey from './pages/SymptomSurveyPage';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -19,6 +20,7 @@ function App() {
     <Route path='/CheckIn' element={<VenueCheckIn />} />
     <Route path='/Countdown' element={<IsolationCountdown />} />
     <Route path='/BookTest' element={<TestBooking username={username} />} />
+    <Route path='/SymptomSurvey' element={<SymptomSurvey />} />
   </Routes>
 }
 
