ad3bf0505b29cc6945bdaa68e8a37de877321561
Replit user <>
2023-03-10 13:54:12 +0000

    Made changes to own and response messaging struct

--- Cases/Cases/Web/src/pages/MessagingServicePage.jsx
+++ Cases/Cases/Web/src/pages/MessagingServicePage.jsx
@@ -5,22 +5,29 @@ import '../styling/MessagingService.css'
 const MessagingService = () => {
 
     function replyMessage(e) {
-        const reply = document.createElement("div")
-        reply.className = "response"
+        const numResponses = 4;
+        const reply = document.createElement("div");
+        const responses = ["this worked", "Could you tell me what the problem is", "I see, let me look into it", "From what you've told me, you likely have COVID-19"];
 
-        if (e.target.messageValue.value === "hello") {
-            reply.innerHTML = "this worked"
-            document.getElementById("MessageBox").appendChild(reply)
+        let childrenCount = document.getElementsByClassName("ownMessage").length - 1;
+        let responseText = responses[childrenCount];
+
+        reply.className = "response";
+        
+        if (childrenCount < numResponses) {
+            document.getElementById("MessageBox").innerHTML += '<div class="response">' + responseText + '</div>'
+            console.log(childrenCount);
         }
     }
 
     function userMessage(e) {
-        const message = document.createElement("div")
+        const message = document.createElement("div");
+        message.className = "ownMessage";
         
-        message.innerHTML = e.target.messageValue.value;
-        message.className = "ownMessage"
-        document.getElementById("MessageBox").appendChild(message);
-        replyMessage(e)
+        //If we don't care about this for the MVP, then this is the short way of doing things 
+        document.getElementById("MessageBox").innerHTML += '<div class="ownMessage">' + e.target.messageValue.value + '</div>';
+
+        replyMessage(e);
     }
 
     function handleMessage(e){
@@ -35,8 +42,12 @@ const MessagingService = () => {
         
         return(
             <form onSubmit={(e) => handleMessage(e)}>
-                <input className="inputText" type="text" id="messageValue" placeholder="Enter message" />
-                <input className="inputLogin" type="submit" value={"Enter"} />
+                <div className="userChatbox">
+                <div className="aaaa"> <input className="inputText" type="text" id="messageValue" placeholder="Enter message" /> </div>
+                <div className="bbbb"> <input className="inputLogin" type="submit" value={"Send"} />   </div>
+                    
+                    
+                </div>
             </form>
         )
     }
